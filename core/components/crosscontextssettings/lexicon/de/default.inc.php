296ce6302bfe929a7c53e795d7b04cad9fa49f35
Thomas Jakobi <buero@treehillstudio.de>
2021-04-15 09:13:41 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: MODX CrossContextsSettings/Standard
    Translate-URL: https://hosted.weblate.org/projects/modx-crosscontextssettings/standard/de/

--- core/components/crosscontextssettings/lexicon/de/default.inc.php
+++ core/components/crosscontextssettings/lexicon/de/default.inc.php
@@ -7,13 +7,13 @@
  */
 $_lang['crosscontextssettings'] = 'CrossContextsSettings';
 $_lang['crosscontextssettings.menu'] = 'CrossContextsSettings';
-$_lang['crosscontextssettings.menu_desc'] = 'Manage the settings of different contexts in one place.';
-$_lang['crosscontextssettings.clearcache'] = 'Clear Cache';
-$_lang['crosscontextssettings.clearcache_desc'] = 'Clear the cache files under the selected contexts. For a large and busy website, this may slow down the website a bit as the caches need to be recreated.';
-$_lang['crosscontextssettings.contextsettings'] = 'Context Settings';
-$_lang['crosscontextssettings.contextsettings_desc'] = 'Manage the settings of different contexts in one place. Edit the value by double clicking the grid\'s cell.';
+$_lang['crosscontextssettings.menu_desc'] = 'Verwalten Sie die Einstellungen von verschiedenen Kontexten an einem Ort.';
+$_lang['crosscontextssettings.clearcache'] = 'Cache löschen';
+$_lang['crosscontextssettings.clearcache_desc'] = 'Löschen Sie die Cache-Dateien unter den ausgewählten Kontexten. Bei einer großen und stark frequentierten Website kann dies die Website ein wenig verlangsamen, da die Caches neu erstellt werden müssen.';
+$_lang['crosscontextssettings.contextsettings'] = 'Kontext-Einstellungen';
+$_lang['crosscontextssettings.contextsettings_desc'] = 'Verwalten Sie die Einstellungen von verschiedenen Kontexten an einem Ort. Bearbeiten Sie den Wert durch einen Doppelklick auf die Zelle des Gitters.';
 $_lang['crosscontextssettings.settings'] = '<i class="icon icon-cog"></i>';
-$_lang['crosscontextssettings.settings_desc'] = 'Edit the settings of ConsentFriend. You can edit the value of a system setting by double-clicking on the \'Value\' table cell or by right-clicking in the table cell.';
-$_lang['crosscontextssettings.context_err_ns'] = 'You need at least select one context.';
-$_lang['crosscontextssettings.contextsetting_err_save'] = 'Could not save setting "[[+key]]" for context "[[+context]]"';
+$_lang['crosscontextssettings.settings_desc'] = 'Bearbeiten Sie die Einstellungen von ConsentFriend. Sie können den Wert einer Systemeinstellung bearbeiten, indem Sie auf die Tabellenzelle \'Wert\' doppelklicken oder mit der rechten Maustaste in die Tabellenzelle klicken.';
+$_lang['crosscontextssettings.context_err_ns'] = 'Sie müssen mindestens einen Kontext auswählen.';
+$_lang['crosscontextssettings.contextsetting_err_save'] = 'Konnte Einstellung "[[+ key]]" für Kontext "[[+ context]]" nicht speichern';
 $_lang['crosscontextssettings.debug_mode'] = 'Debug Mode';
