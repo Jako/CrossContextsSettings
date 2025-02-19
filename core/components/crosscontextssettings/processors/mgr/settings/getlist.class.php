c15ac4f1e49c8856533e5f31550f16d07a23fbdb
Thomas Jakobi <thomas.jakobi@partout.info>
2025-02-19 10:57:50 +0100

    Check the namespaced settings permission for the listing of the system settings too

--- core/components/crosscontextssettings/processors/mgr/settings/getlist.class.php
+++ core/components/crosscontextssettings/processors/mgr/settings/getlist.class.php
@@ -13,11 +13,23 @@ if (file_exists(MODX_PROCESSORS_PATH . 'system/settings/getlist.class.php')) {
     class_alias(\MODX\Revolution\Processors\System\Settings\GetList::class, \modSystemSettingsGetListProcessor::class);
 }
 
+/**
+ * Class CrossContextsSettingsSystemSettingsGetlistProcessor
+ */
 class CrossContextsSettingsSystemSettingsGetlistProcessor extends modSystemSettingsGetListProcessor
 {
     public $languageTopics = ['setting', 'namespace', 'crosscontextssettings:setting'];
     public $objectType = 'setting';
     public $primaryKeyField = 'key';
+
+    /**
+     * {@inheritDoc}
+     * @return bool
+     */
+    public function checkPermissions()
+    {
+        return !empty($this->permission) ? $this->modx->hasPermission($this->permission) || $this->modx->hasPermission('crosscontextssettings_' . $this->permission) : true;
+    }
 
     /**
      * {@inheritDoc}
