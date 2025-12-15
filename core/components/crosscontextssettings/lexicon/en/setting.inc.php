114ec6d7361bb36c350322e50c58408c1f4150b6
Thomas Jakobi <thomas.jakobi@partout.info>
2025-12-15 08:58:37 +0100

    Fix lexicon typo

--- core/components/crosscontextssettings/lexicon/en/setting.inc.php
+++ core/components/crosscontextssettings/lexicon/en/setting.inc.php
@@ -8,6 +8,6 @@
 $_lang['setting_crosscontextssettings.clear_cache'] = 'Clear Context Cache';
 $_lang['setting_crosscontextssettings.clear_cache_desc'] = 'Clear the context cache after modifying an entry.';
 $_lang['setting_crosscontextssettings.contexts'] = 'Contexts';
-$_lang['setting_crosscontextssettings.contexts_desc'] = 'Comma separated list of contexts displayed in the custom manager page. No value means that all contexts are displayed.';
+$_lang['setting_crosscontextssettings.contexts_desc'] = 'Comma-separated list of contexts displayed in the custom manager page. No value means that all contexts are displayed.';
 $_lang['setting_crosscontextssettings.debug'] = 'Debug';
 $_lang['setting_crosscontextssettings.debug_desc'] = 'Log debug information in the MODX error log.';
